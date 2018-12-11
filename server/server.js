cbb5d7a7458c4d6de3717c6397a1506dc3479260
Steven Choi <steven.m.choi@gmail.com>
2018-12-10 23:59:24 -0800

    Connect lyrical-graphql to mongodb

--- server/server.js
+++ server/server.js
@@ -8,22 +8,26 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI =
+	'mongodb://steven:password1@ds015899.mlab.com:15899/lyricaldb';
 if (!MONGO_URI) {
-  throw new Error('You must provide a MongoLab URI');
+	throw new Error('You must provide a MongoLab URI');
 }
 
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URI);
 mongoose.connection
-    .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error));
+	.once('open', () => console.log('Connected to MongoLab instance.'))
+	.on('error', (error) => console.log('Error connecting to MongoLab:', error));
 
 app.use(bodyParser.json());
-app.use('/graphql', expressGraphQL({
-  schema,
-  graphiql: true
-}));
+app.use(
+	'/graphql',
+	expressGraphQL({
+		schema,
+		graphiql: true,
+	})
+);
 
 const webpackMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');

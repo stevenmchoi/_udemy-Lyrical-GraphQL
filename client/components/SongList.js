851ee9624ab203bb91c79e7863b55a877134fad4
Steven Choi <steven.m.choi@gmail.com>
2018-12-17 21:10:30 -0800

    Render song titles in SongList

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class SongList extends Component {
+	renderSongs() {
+		const songs = this.props.data.songs;
+		// console.log(data);
+
+		if (songs) {
+			return songs.map((song) => song.title);
+		}
+	}
+
 	render() {
-		return <div>SongList</div>;
+		return <div>{this.renderSongs()}</div>;
 	}
 }
 
-export default SongList;
+const query = gql`
+	{
+		songs {
+			title
+		}
+	}
+`;
+
+export default graphql(query)(SongList);

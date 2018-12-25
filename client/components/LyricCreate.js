37858920ffc8c6c9af8c11cbfc1c88c0228409ae
Steven Choi <steven.m.choi@gmail.com>
2018-12-24 20:42:55 -0800

    Change and clear state var content

--- client/components/LyricCreate.js
+++ client/components/LyricCreate.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 
 class LyricCreate extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { content: '' };
+	}
+
+	onSubmit(event) {
+		event.preventDefault();
+		console.log(event);
+		this.setState({ content: '' });
+	}
+
 	render() {
 		return (
-			<form>
+			<form onSubmit={this.onSubmit.bind(this)}>
 				<lable>Add a Lyric</lable>
-				<input />
+				<input
+					value={this.state.content}
+					onChange={(event) => this.setState({ content: event.target.value })}
+				/>
 			</form>
 		);
 	}

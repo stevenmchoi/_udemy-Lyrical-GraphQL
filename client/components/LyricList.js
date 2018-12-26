51f1df3393f45d55967bbacb2ccf948513cea4c4
Steven Choi <steven.m.choi@gmail.com>
2018-12-25 16:13:42 -0800

    Style vote-box for likes

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -8,17 +8,18 @@ class LyricList extends Component {
 			return (
 				<li key={id} className="collection-item">
 					{content}
-					<i onClick={() => this.onLike(id)} className="material-icons">
-						thumb_up
-					</i>
-					{likes}
+					<div className="vote-box">
+						<i onClick={() => this.onLike(id)} className="material-icons">
+							thumb_up
+						</i>
+						{likes}
+					</div>
 				</li>
 			);
 		});
 	}
 
 	onLike(id) {
-		console.log(id);
 		this.props.mutate({
 			variables: { id },
 		});
@@ -33,12 +34,7 @@ const mutation = gql`
 	mutation LikeLyric($id: ID) {
 		likeLyric(id: $id) {
 			id
-			content
 			likes
-			song {
-				id
-				title
-			}
 		}
 	}
 `;

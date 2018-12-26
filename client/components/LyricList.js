8f7d32fb7617f16fa92abe323beb086670783b3a
Steven Choi <steven.m.choi@gmail.com>
2018-12-25 16:30:49 -0800

    Update UI faster with optimisticResponse

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -9,7 +9,10 @@ class LyricList extends Component {
 				<li key={id} className="collection-item">
 					{content}
 					<div className="vote-box">
-						<i onClick={() => this.onLike(id)} className="material-icons">
+						<i
+							onClick={() => this.onLike(id, likes)}
+							className="material-icons"
+						>
 							thumb_up
 						</i>
 						{likes}
@@ -19,9 +22,17 @@ class LyricList extends Component {
 		});
 	}
 
-	onLike(id) {
+	onLike(id, likes) {
 		this.props.mutate({
 			variables: { id },
+			optimisticResponse: {
+				__typename: 'Mutation',
+				likeLyric: {
+					id,
+					__typename: 'LyricType',
+					likes: likes + 1,
+				},
+			},
 		});
 	}
 
